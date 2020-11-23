a75da1f60b6773850bafa065c7c5569cb67725d1
novastosha <galliigotu@gmail.com>
2020-11-23 08:43:07 +0100

    Token is Public this is a test bot...

--- ubot/index.js
+++ ubot/index.js
@@ -1,1 +1,11 @@
 const discord = require('discord.js');
+const client = new discord.Client();
+
+function main() {
+    console.log("Bot starting...");
+    client.on("ready", () =>{
+      console.log("Client Ready!");
+    });
+}
+client.login('NzgwMzM2NDUxMjIwNTM3MzY1.X7tm8w.6z5tugL4obfblpfnRFlwrMPZeQQ')
+main()
